be5b01214a13e5c71fc20f18445557a20f6c9dc5
daxilbhuva <shaktijay40@gmail.com>
2026-06-19 12:59:51 +0530

    Add files via upload

---
+++ max.java
@@ -0,0 +1,19 @@
+class max
+{
+	public static void main(String args[])
+	{
+		int a=54,b=68,c=85;
+		if(a>b && a>c)
+		{
+			System.out.println("a is max");
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("b is max");
+		}
+		else
+		{
+			System.out.println("c is max");
+		}
+	}
+}
---
+++ min.java
@@ -0,0 +1,19 @@
+class min
+{
+	public static void main(String args[])
+	{
+		int a=54,b=68,c=85;
+		if(a<b && a<c)
+		{
+			System.out.println("a is min");
+		}
+		else if(b<a && b<c)
+		{
+			System.out.println("b is min");
+		}
+		else
+		{
+			System.out.println("c is min");
+		}
+	}
+}
